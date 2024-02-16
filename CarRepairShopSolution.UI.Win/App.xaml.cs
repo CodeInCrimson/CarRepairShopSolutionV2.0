7977d46ceb3f7bfec49530e8d09104a1a74e11ec
Victor T <victor.tambur@globallogic.com>
2024-02-16 22:23:10 +0200

    Added logging and appsettings.json properties.

--- CarRepairShopSolution.UI.Win/App.xaml.cs
+++ CarRepairShopSolution.UI.Win/App.xaml.cs
@@ -1,14 +1,47 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿// <copyright file="App.xaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace CarRepairShopSolution.UI.Win;
 
-namespace CarRepairShopSolution.UI.Win
+using System.Diagnostics;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+/// <summary>
+/// Interaction logic for App.xaml.
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public static IConfiguration? Config { get; private set; }
+
+    public App()
     {
+        Config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(Config)
+            .CreateLogger();
+
+        // Ensure any configuration errors in Serilog are caught
+        Serilog.Debugging.SelfLog.Enable(msg => Debug.WriteLine(msg));
+
+        Log.Information("Application Starting Up");
     }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        Log.Information("Application starting up");
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        Log.Information("Application Shutting Down");
+        Log.CloseAndFlush();
+        base.OnExit(e);
+    }
 }
