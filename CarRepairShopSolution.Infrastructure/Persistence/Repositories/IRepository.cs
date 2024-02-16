b297362b4a0265c70ed5e45430b1d1d903e02be7
Victor T <victor.tambur@globallogic.com>
2024-02-17 00:05:15 +0200

    Add repository interface and class.

---
+++ CarRepairShopSolution.Infrastructure/Persistence/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRepairShopSolution.Infrastructure.Persistence.Repositories;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<List<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
---
+++ CarRepairShopSolution.Infrastructure/Persistence/Repositories/Repository.cs
@@ -0,0 +1,26 @@
+﻿namespace CarRepairShopSolution.Infrastructure.Persistence.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using CarRepairShopSolution.Infrastructure.Persistence.DatabaseContextInit;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
+
+    public async Task DeleteAsync(T entity) => _context.Set<T>().Remove(entity);
+
+    public async Task<List<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+
+    public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FindAsync(id);
+
+    public async Task UpdateAsync(T entity) => _context.Set<T>().Update(entity);
+}
